d6dab77c965ba6508466cff914bb588c3e3eab5a
Camila Cazzaniga <ccazzanigaux@gmail.com>
2026-08-11 19:58:45 -0300

    tabla de novedades

--- back/routes/admin/novedades.js
+++ back/routes/admin/novedades.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('./../../models/novedadesModel');
 
 /* lista de novedades */
 router.get('/', async function (req, res, next) {
+  var novedades = await novedadesModel.getNovedades();
   res.render('admin/novedades', { //novedades.hbs
     layout: 'admin/layout',
-    usuario: req.session.nombre
+    usuario: req.session.nombre,
+    novedades
   });
 });
 
