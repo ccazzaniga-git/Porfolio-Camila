1ddd5dac4c6dcb16e4bc8565294da7beb53bb2ec
Camila Cazzaniga <ccazzanigaux@gmail.com>
2026-08-24 21:53:54 -0300

    img

--- back/routes/admin/productos.js
+++ back/routes/admin/productos.js
@@ -2,11 +2,34 @@ var express = require('express');
 var router = express.Router();
 var productosModel = require('./../../models/productosModel');
 
-/* lista de productos */
+var util = require('util');
+var cloudinary = require('cloudinary').v2;
+const uploader = util.promisify(cloudinary.uploader.upload);
+
+const destroy = util.promisify(cloudinary.uploader.destroy);
+
+
+// lista de productos 
 router.get('/', async function (req, res, next) {
 
   var productos = await productosModel.getProductos();
-  
+
+  productos = productos.map(producto => {
+    if (producto.img_id) {
+      const imagen = cloudinary.image(producto.img_id, {
+        width: 100,
+        height: 100,
+        crop: "fill"
+      });
+      return { ...producto,
+        imagen };
+    } else {
+      return { ...producto,
+        imagen: '' 
+      };
+    }
+  });
+
   res.render('admin/productos', { //productos.hbs
     layout: 'admin/layout',
     usuario: req.session.nombre,
@@ -15,6 +38,8 @@ router.get('/', async function (req, res, next) {
 });
 
 
+
+// AGREGAR PRODUCTO
 router.get('/agregar', (req, res, next) => {
   res.render('admin/agregar', {  //agregar.hbs
     layout: 'admin/layout'
@@ -24,9 +49,23 @@ router.get('/agregar', (req, res, next) => {
 
 router.post('/agregar', async (req, res, next) => {
   try {
+
+    var img_id = '';
+
+    if (req.files && Object.keys(req.files).length > 0) {
+      imagen = req.files.imagen;
+      img_id = (await uploader(imagen.tempFilePath)).public_id;
+    }
+
     if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != "" && req.body.precio != "") {
-      await productosModel.insertProducto(req.body);
+
+      await productosModel.insertProducto({
+        ...req.body,
+        img_id
+      });
+
       res.redirect('/admin/productos');
+
     } else {
       res.render('admin/agregar', {
         layout: 'admin/layout',
@@ -44,14 +83,22 @@ router.post('/agregar', async (req, res, next) => {
   }
 });
 
+
+// ELIMINAR PRODUCTO
 router.get('/eliminar/:id', async (req, res, next) => {
   var id = req.params.id;
+
+  let producto = await productosModel.getProductoById(id);
+
+  if (producto.img_id) {
+    await (destroy(producto.img_id));
+  }
   await productosModel.deleteProductoById(id);
   res.redirect('/admin/productos');
 });
 
 
-/*para listar UNA SOLA NOVEDAD - MODIFICAR*/
+// para listar UNA SOLA NOVEDAD - MODIFICAR*/
 router.get('/modificar/:id', async (req, res, next) => {
   var id = req.params.id;
   console.log(id);
@@ -66,10 +113,31 @@ router.get('/modificar/:id', async (req, res, next) => {
 
 router.post('/modificar', async (req, res, next) => {
   try {
+
+    let img_id = req.body.img_original;
+
+    let borrar_img_vieja = false;
+
+    if (req.body.img_delete === "1") {
+      img_id = null;
+      borrar_img_vieja = true;
+    } else {
+      if (req.files && Object.keys(req.files).length > 0) {
+        imagen = req.files.imagen;
+        img_id = (await uploader(imagen.tempFilePath)).public_id;
+        borrar_img_vieja = true;
+      }
+    }
+
+    if (borrar_img_vieja && req.body.img_original) {
+      await (destroy(req.body.img_original));
+    }
+
     var obj = {
       titulo: req.body.titulo,
       subtitulo: req.body.subtitulo,
       cuerpo: req.body.cuerpo,
+      img_id,
       precio: req.body.precio
     };
     console.log(obj);
@@ -87,5 +155,6 @@ router.post('/modificar', async (req, res, next) => {
     });
   }
 });
+
 
 module.exports = router;
